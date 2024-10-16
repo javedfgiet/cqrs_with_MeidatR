2bd6f87cc7a3de34bf08837a6b309e4b895ade37
Adminuser <javedfgiet@gmail.com>
2024-10-16 18:20:48 +0000

    Added MediatR with cqrs for Query

---
+++ cqrs_with_Mediatr/DemoLibrary/Handler/GetPersonListHandler.cs
@@ -0,0 +1,25 @@
+﻿using DemoLibrary.DataAccess;
+using DemoLibrary.Models;
+using DemoLibrary.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.Handler
+{
+    public class GetPersonListHandler : IRequestHandler<GetPersonListQuery, List<PersonModel>>
+    {
+        private readonly IDataAccess _data;
+        public GetPersonListHandler(IDataAccess dataAccess)
+        {
+            _data = dataAccess;
+        }
+        public Task<List<PersonModel>> Handle(GetPersonListQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_data.GetPeople());
+        }
+    }
+}
